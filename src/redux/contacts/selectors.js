7694065679299fb4d95675c042a497412d0ee233
Ladamenko <ladamenko@gmail.com>
2024-06-21 12:35:33 +0300

    Done

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -9,9 +9,11 @@ export const selectError = state => state.contacts.error;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
-  (contacts, nameFilter) => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+  (contacts, filter) => {
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.includes(filter)
     );
   }
 );
